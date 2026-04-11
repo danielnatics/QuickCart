ee1019a6efd55a3fad41c8ee94aace852d2bb1c4
Egwuatu Daniel <danielegwuatu123@gmail.com>
2026-04-11 21:59:47 +0100

    update

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import HeaderSlider from "@/components/HeaderSlider";
 import HomeProducts from "@/components/HomeProducts";
-import Banner from "@/components/Banner";
+// import Banner from "@/components/Banner";
 import NewsLetter from "@/components/NewsLetter";
 import FeaturedProduct from "@/components/FeaturedProduct";
 import Navbar from "@/components/Navbar";
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,12 +7,7 @@ const Footer = () => {
     <footer>
       <div className="flex flex-col md:flex-row items-start justify-center px-6 md:px-16 lg:px-32 gap-10 py-14 border-b border-gray-500/30 text-gray-500">
         <div className="w-4/5">
-          <Image
-            className="w-28 md:w-32"
-            src={assets.logo}
-            alt="logo"
-            // style={{ width: "50px" }}
-          />
+          <div>HezTec</div>
           <p className="mt-6 text-sm">
             Your trusted source for high-quality electronic components,
             microcontrollers, sensors, displays, and modules. We provide
@@ -55,13 +50,13 @@ const Footer = () => {
             <h2 className="font-medium text-gray-900 mb-5">Get in touch</h2>
             <div className="text-sm space-y-2">
               <p>+2348130123588</p>
-              <p>danchibu22@gmail.com</p>
+              <p>getheztec@gmail.com</p>
             </div>
           </div>
         </div>
       </div>
       <p className="py-4 text-center text-xs md:text-sm">
-        Copyright 2025 © HezTech All Right Reserved.
+       © {new Date().getFullYear()} HezTec. All rights reserved.
       </p>
     </footer>
   );
